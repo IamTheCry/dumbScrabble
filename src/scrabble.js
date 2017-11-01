a73bb1ad60b07f3265a1ab6847fd382830eae50f
Corey <cry@Coreys-MacBook-Air.local>
2017-11-01 11:31:12 -0700

    Psuedo solution to dictionary matching

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,2 +1,16 @@
-// send letters to server
+// find words given 7 letters
+
+// i.e. var string = "abcdefg"
+
+// input: a 7 character string
+// output: words you can spell with any combination of those letters
+// constraints: nothing listed
+// edge cases: No words, or just a single letter
+
+// treat the string as an array
+// go through the dictionary and treat each word as an array
+// go through every word 7 characters or less
+  // for each word, check if it contains the letters submitted
+  // if every letter in the word matches with the letters submitted, push to result array
+// return the result array 
 
